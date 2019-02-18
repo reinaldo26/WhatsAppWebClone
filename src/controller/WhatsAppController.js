d6bd3f244186416f9169864efe69744f08bec9ca
Reinaldo Oliveira <reinaldo.brooke@gmail.com>
2019-02-18 12:38:39 -0300

    criando eventos para abrir/fechar os painéis 'editar perfil' e 'adicionar contato'

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -3,6 +3,7 @@ class WhatsAppController {
     constructor(){
         this.elementsPrototype();
         this.loadElements();
+        this.initEvents();
     }
 
     elementsPrototype(){
@@ -61,5 +62,37 @@ class WhatsAppController {
         document.querySelectorAll('[id]').forEach((element)=>{
             this.el[Format.getCamelCase(element.id)] = element;
         });
+    }
+
+    initEvents(){
+        this.el.myPhoto.on('click', (e)=>{
+            this.closeAllLeftPanel();
+            this.el.panelEditProfile.show();
+            setTimeout(() => {
+                this.el.panelEditProfile.addClass('open');
+            }, 300);
+        });
+
+        this.el.btnNewContact.on('click', (e)=>{
+            this.closeAllLeftPanel();
+            this.el.panelAddContact.show();
+            setTimeout(() => {
+                this.el.panelAddContact.addClass('open');
+            }, 300);
+            
+        });
+
+        this.el.btnClosePanelEditProfile.on('click', (e)=>{
+            this.el.panelEditProfile.removeClass('open');
+        });
+
+        this.el.btnClosePanelAddContact.on('click', (e)=>{
+            this.el.panelAddContact.removeClass('open');
+        });
+    }
+
+    closeAllLeftPanel(){
+        this.el.panelAddContact.hide();
+        this.el.panelEditProfile.hide();
     }
 }
