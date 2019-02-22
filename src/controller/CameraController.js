c611fb42804e4e96fd9c106e0ed54227964113b1
Reinaldo Oliveira <reinaldo.brooke@gmail.com>
2019-02-22 10:32:41 -0300

    tirando fotografias da câmera com canvas

--- src/controller/CameraController.js
+++ src/controller/CameraController.js
@@ -5,6 +5,7 @@ export class CameraController {
         navigator.mediaDevices.getUserMedia({
             video: true
         }).then((stream)=>{
+            this._stream = stream;
             this._videoEl.srcObject = stream;
             this._videoEl.play();
         }).catch((err)=>{
@@ -12,4 +13,19 @@ export class CameraController {
         });
     }
     
+    stop(){
+        this._stream.getTracks().forEach((track)=>{
+            track.stop();
+        });
+    }
+
+    takePicture(mimeType = 'imege/png'){
+        let canvas = document.createElement('canvas');
+        canvas.setAttribute('height', this._videoEl.videoHeight);
+        canvas.setAttribute('width', this._videoEl.videoWidth);
+
+        let context = canvas.getContext('2d');
+        context.drawImage(this._videoEl, 0, 0, canvas.width, canvas.height);
+        return canvas.toDataURL(mimeType);
+    }
 }
